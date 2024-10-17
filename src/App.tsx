89a87f07a56b00d3793f977d5f7daf7e69e903dd
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-10-17 13:35:37 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, message } from "antd";
+import { Button, Input, message, Spin } from "antd";
 import {
   Column,
   FullScreen,
@@ -12,6 +12,7 @@ import { useState } from "react";
 function App() {
   const [key, setKey] = useState("");
   const [messageApi, contextHolder] = message.useMessage();
+  const [uploading, setUploading] = useState(false);
   const success = (msg: string) => {
     messageApi.open({
       type: "success",
@@ -23,6 +24,26 @@ function App() {
       type: "error",
       content: msg,
     });
+  };
+
+  const handleActivation = async () => {
+    setUploading(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 500)); // 延迟500毫秒
+      const temp = await window.pyloid.custom.bindDeviceID(key);
+      if (temp === 1) {
+        success("激活成功");
+      } else if (temp === 0) {
+        error("激活失败");
+      } else if (temp === -1) {
+        error("密钥已使用, 不进行任何操作");
+      } else {
+        error("密钥无效");
+      }
+    } catch (err) {
+      error("激活过程中发生错误");
+    }
+    setUploading(false);
   };
 
   return (
@@ -40,23 +61,8 @@ function App() {
               onChange={(e) => setKey(e.target.value)}
             />
             {hspace20}
-            <Button
-              onClick={async () => {
-                const temp = await window.pyloid.custom.bindDeviceID(key);
-                if (temp==1) {
-                  success("激活成功");
-                } else if (temp==0) {
-                  error("激活失败");
-                }
-                else if (temp==-1){
-                  error("密钥已使用, 不进行任何操作")
-                }
-                else {
-                  error("密钥无效");
-                }
-              }}
-            >
-              激活
+            <Button disabled={uploading} onClick={handleActivation}>
+              {uploading ? <Spin size="small" /> : "激活"}
             </Button>
           </Row>
         </Sizedbox>
